87bf1df67d3b08f0a8ee46900d6d31745933b24b
GGS <85903509+Guilherme-Goncalves-de-Souza@users.noreply.github.com>
2022-12-27 21:20:11 -0300

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let menuMobile = document.querySelector(".menu-mobile")
+let menuDesktop = document.querySelector(".menu")
+
+function openMenu(){
+    menuMobile.classList.remove("none")
+}
+
+function menuClose(){
+    menuMobile.classList.add("none")
+}
